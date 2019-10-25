f4bac671e3dba8f20152fa2890bafc3a3d851df1
SujeethJinesh <sujeethjinesh@gmail.com>
2019-10-24 21:57:39 -0700

    Introduce Email Intent & Bug Fix

--- app/src/main/java/com/sujeeth/calhacksstarterapp/MainActivity.java
+++ app/src/main/java/com/sujeeth/calhacksstarterapp/MainActivity.java
@@ -1,13 +1,48 @@
 package com.sujeeth.calhacksstarterapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nameEditText;
+    EditText emailEditText;
+
+    Button emailButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameEditText = findViewById(R.id.name_edit_text);
+        emailEditText = findViewById(R.id.email_edit_text);
+
+        emailButton = findViewById(R.id.email_button);
+
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"sujeethjinesh@gmail.com"});
+                intent.putExtra(Intent.EXTRA_TEXT, "Hi I'm " + nameEditText.getText() + "\n" + emailEditText.getText());
+
+                if(intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, 0);
+                }
+            }
+        });
+    }
+
+    public void goToFacebookProfile(View view) {
+        Uri uri = Uri.parse("https://www.facebook.com/profile.php?id=100024908682333"); // missing 'http://' will cause crash
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
