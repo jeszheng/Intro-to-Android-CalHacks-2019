becb2714dcc695558a707da2310dce67c1547d31
SujeethJinesh <sujeethjinesh@gmail.com>
2019-10-24 22:10:49 -0700

    9. Add Write to Firebase

--- app/src/main/java/com/sujeeth/calhacksstarterapp/MainActivity.java
+++ app/src/main/java/com/sujeeth/calhacksstarterapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -47,25 +49,42 @@ public class MainActivity extends AppCompatActivity {
         });
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        final DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
 
         // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                emailEditText.setText(value); // example of data flow
+//                Log.d("MyRefDataChanged", "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w("MyRefFailedToReadValue", "Failed to read value.", error.toException());
+//            }
+//        });
+
+        // Write to database
+        emailEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                emailEditText.setText(value); // example of data flow
-                Log.d("MyRefDataChanged", "Value is: " + value);
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("MyRefFailedToReadValue", "Failed to read value.", error.toException());
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                myRef.setValue(editable.toString());
             }
         });
     }
