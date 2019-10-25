82829f92ac01796dbef979d87582da8d674c9c7b
SujeethJinesh <sujeethjinesh@gmail.com>
2019-10-24 22:05:19 -0700

    8. Add Firebase

--- app/src/main/java/com/sujeeth/calhacksstarterapp/MainActivity.java
+++ app/src/main/java/com/sujeeth/calhacksstarterapp/MainActivity.java
@@ -4,9 +4,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +43,29 @@ public class MainActivity extends AppCompatActivity {
                 if(intent.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(intent, 0);
                 }
+            }
+        });
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                emailEditText.setText(value); // example of data flow
+                Log.d("MyRefDataChanged", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("MyRefFailedToReadValue", "Failed to read value.", error.toException());
             }
         });
     }
